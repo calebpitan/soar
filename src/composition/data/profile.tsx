703e5049802c27ee0fd24ace60ada7d15f9f1170
Caleb Adepitan <calebpitan@gmail.com>
2025-02-17 21:43:52 +0100

    update: add mock for profile data

---
+++ src/composition/data/profile.tsx
@@ -0,0 +1,17 @@
+import { EditProfileSettingsProps } from '../settings'
+
+export const profiles: Array<EditProfileSettingsProps['profile']> = [
+  {
+    avatar: '/images/avatar.png',
+    firstName: 'Charlene',
+    lastName: 'Reed',
+    username: 'charlene_reed',
+    email: 'charlenereed@gmail.com',
+    dateOfBirth: '1989-10-11',
+    presentAddress: 'Mountain View, California, USA',
+    permanentAddress: 'Atlanta, Georgia, USA',
+    city: 'Atlanta',
+    postalCode: '415-789',
+    country: 'USA',
+  },
+]
