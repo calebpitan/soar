b1c8b8ae8132e42c77880efd4ff9acb10679e1bf
Caleb Adepitan <calebpitan@gmail.com>
2025-02-17 23:48:26 +0100

    update: add sleep function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sleep(ms: number) {
+  return new Promise<number>((resolve) => {
+    setTimeout(() => resolve(ms), ms)
+  })
+}
