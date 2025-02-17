63b4610f46e93e3f958830581c4a17b1d3bc41ef
Caleb Adepitan <calebpitan@gmail.com>
2025-02-17 12:05:49 +0100

    update: add util functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,25 @@ export function isFn(v: unknown): v is CallableFunction {
 export function clamp(min: number, val: number, max: number) {
   return Math.max(min, Math.min(val, max))
 }
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+export function monthToValue(month: string) {
+  return months.indexOf(month)
+}
+
+export function valueToMonth(val: number) {
+  return months[val]
+}
