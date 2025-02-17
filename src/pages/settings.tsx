4189f87750bb8989bfa915a90da86b770c227afa
Caleb Adepitan <calebpitan@gmail.com>
2025-02-17 22:40:45 +0100

    refactor: modify tab component to fit design

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -8,15 +8,27 @@ import { cn } from '@/lib/utils'
 import type { PassedProps } from './_app'
 
 export default function Settings({ fonts }: PassedProps) {
+  const tabsTriggerExtras = {
+    className: `lg:w-30 lg:text-base data-[state=active]:shadow-none relative \
+      data-[state=active]:after:absolute data-[state=active]:after:-left-1 data-[state=active]:after:-bottom-1 \
+      data-[state=active]:after:w-full data-[state=active]:after:h-[3px] lg:data-[state=active]:after:h-[5px] \
+      data-[state=active]:after:bg-black data-[state=active]:after:rounded-md`,
+  }
   return (
     <div className={cn(fonts.inter.className)}>
       <AppLayout className="bg-background-alt">
         <Paper className="p-4 lg:p-6">
           <Tabs defaultValue="edit-profile">
-            <TabsList>
-              <TabsTrigger value="edit-profile">Edit Profile</TabsTrigger>
-              <TabsTrigger value="preferences">Preferences</TabsTrigger>
-              <TabsTrigger value="security">Security</TabsTrigger>
+            <TabsList className="bg-background border-b overflow-y-hidden rounded-none w-full justify-between lg:justify-start lg:space-x-8 xl:space-x-12">
+              <TabsTrigger {...tabsTriggerExtras} value="edit-profile">
+                Edit Profile
+              </TabsTrigger>
+              <TabsTrigger {...tabsTriggerExtras} value="preferences">
+                Preferences
+              </TabsTrigger>
+              <TabsTrigger {...tabsTriggerExtras} value="security">
+                Security
+              </TabsTrigger>
             </TabsList>
 
             <TabsContent value="edit-profile">
